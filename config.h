f1cd828ee2f14bbf6bc7e54af416845ddce59e81
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2020-07-01 12:58:37 +0200

    fix spaces

--- config.h
+++ config.h
@@ -12,12 +12,12 @@
  */
 
 static const Block blocks[] = {
-	/* icon / native command / external command / update interval / update signal */
-	{ "🐧 ",        0,          "uname -r",    0,              10 },
-	{ "🌡 ",      0,          "sensors | awk '/Core 0/{print $3}'",    5,              0 },
-	{ "♪ ",        0,          "pamixer --get-volume-human",    0,              10 },
-	{ "⌨ ",         0,          "get_keyboard",                  0,              12 },
-	{ "",        CMD_TIME,            0,                60,             9 }
+	/* icon /   native command  /       external command        /update interval / update signal */
+	{ "🐧 ",        0,          "uname -r",                              0,              10 },
+	{ "🌡 ",         0,          "sensors | awk '/Core 0/{print $3}'",    5,              0 },
+	{ "♪ ",         0,          "pamixer --get-volume-human",            0,              10 },
+	{ "⌨ ",         0,          "get_keyboard",                          0,              12 },
+	{ "",        CMD_TIME,            0,                                 60,             9 }
 };
 
 /* Sets delimeter between status commands */
